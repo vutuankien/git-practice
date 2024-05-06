0ab74fe471ee8a2829d99d100c918904f64eb218
vũ tuấn kiên <117794130+vutuankien@users.noreply.github.com>
2024-05-06 23:55:32 +0700

    task_1: done validate

--- develop_part/app.js
+++ develop_part/app.js
@@ -1,69 +1,82 @@
-function validateLoginForm() {
-    const username = document.querySelector("#username");
-    const password = document.querySelector("#password");
-    const button = document.querySelector(".btn");
-    const message = document.querySelector(".-regis");
-    const usernameMessage = document.querySelector(".message-username");
+//Rgex Pattern
+const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?])[a-zA-Z0-9!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?]{6,}$/;
+const usernameRegex = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
+const emialRegex = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/
 
-    const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?])[a-zA-Z0-9!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?]{6,}$/;
-    const usernameRegex = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
-    button.addEventListener("click", () => {
-        if (password.value.match(passwordRegex)) {
-            message.innerText = "";
-            message.classList.remove("isActive");
-            console.log(message);
-        } else {
-            message.innerText = "Wrong Password";
-            message.classList.add("isActive");
-            console.log(message);
-        }
+const username_log = document.querySelector("#username");
+const password_log = document.querySelector("#password");
+const button_log = document.querySelector(".btn");
+const messagePassword = document.querySelector(".message-password");
+const usernameMessage = document.querySelector(".message-username");
 
-        if (username.value.match(usernameRegex)) {
-            usernameMessage.innerText = "";
-            usernameMessage.classList.remove("isActive");
-            console.log(usernameMessage);
-        } else {
-            usernameMessage.innerText = "Wrong Username";
-            usernameMessage.classList.add("isActive");
-            console.log(usernameMessage);
-        }
-    });
-}
-function validateRegisterForm() {
-    const username = document.querySelector("#username-regis");
-    const password = document.querySelector("#password-regis");
-    const button = document.querySelector(".btn");
-    const message = document.querySelector(".message-password-regis");
-    const usernameMessage = document.querySelector(".message-username-regis");
+button_log.addEventListener("click", () => {
+    if (password_log.value.match(passwordRegex)) {
+        messagePassword.innerText = "";
+        messagePassword.classList.remove("isActive");
+        console.log(messagePassword);
+    } else {
+        messagePassword.innerText = "Wrong Password";
+        messagePassword.classList.add("isActive");
+        console.log(messagePassword);
+    }
 
-    const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?])[a-zA-Z0-9!@#\$%\^&*\(\)_+\-=\[\]\{\};':"\\|,.<>\/?]{6,}$/;
-    const usernameRegex = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
-    button.addEventListener("click", () => {
-        if (password.value.match(passwordRegex)) {
-            message.innerText = "";
-            message.classList.remove("isActive");
-            console.log(message);
-        } else {
-            message.innerText = "Wrong Password";
-            message.classList.add("isActive");
-            console.log(message);
-        }
+    if (username_log.value.match(usernameRegex)) {
+        usernameMessage.innerText = "";
+        usernameMessage.classList.remove("isActive");
+        console.log(usernameMessage);
+    } else {
+        usernameMessage.innerText = "Wrong Username";
+        usernameMessage.classList.add("isActive");
+        console.log(usernameMessage);
+    }
+});
 
-        if (username.value.match(usernameRegex)) {
-            usernameMessage.innerText = "";
-            usernameMessage.classList.remove("isActive");
-            console.log(usernameMessage);
-        } else {
-            usernameMessage.innerText = "Wrong Username";
-            usernameMessage.classList.add("isActive");
-            console.log(usernameMessage);
-        }
-    });
-}
-validateLoginForm();
-validateRegisterForm();
+//registation form validation
+const username_res = document.querySelector("#username-regis");
+const password_res = document.querySelector("#password-regis");
+const button_res = document.querySelector(".btn_res");
+const email = document.querySelector('#email');
+const passwordMessage_res = document.querySelector(".message-password-regis");
+const usernameMessage_res = document.querySelector(".message-username-regis");
+const emailMessage  = document.querySelector('.message-email-regis');
+
+button_res.addEventListener("click", () => {
+    //validate password
+    if (password_res.value.match(passwordRegex)) {
+        passwordMessage_res.innerText = "";
+        passwordMessage_res.classList.remove("isActive");
+        console.log(passwordMessage_res);
+    } else {
+        passwordMessage_res.innerText = "Wrong Password";
+        passwordMessage_res.classList.add("isActive");
+        console.log(passwordMessage_res);
+    }
+
+    //validate username
+    if (username_res.value.match(usernameRegex)) {
+        usernameMessage_res.innerText = "";
+        usernameMessage_res.classList.remove("isActive");
+        console.log(usernameMessage_res);
+    } else {
+        usernameMessage_res.innerText = "Wrong Username";
+        usernameMessage_res.classList.add("isActive");
+        console.log(usernameMessage_res);
+    }
+//Kien392004@
+    //validate Email
+    if(email.value.match(emialRegex)){
+        emailMessage.innerText = "";
+        emailMessage.classList.remove('isActive');
+        console.log(emailMessage);
+    }else{
+        // email.value = '';
+        emailMessage.innerText = "Email should be like abc@gmail.com";
+        emailMessage.classList.add('isActive');
+        console.log(emailMessage);
+    }
+});
+
 //switch form
 const login = document.querySelector(".login");
 const register = document.querySelector(".signup");
