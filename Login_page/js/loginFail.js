7c0064403d610da5c2559c25286d7e771d2dc493
vũ tuấn kiên <117794130+vutuankien@users.noreply.github.com>
2024-05-11 01:42:59 +0700

    refresh page

--- Login_page/js/loginFail.js
+++
@@ -1,6 +0,0 @@
-const forget_link = document.querySelector('.forget-text');
-forget_link.addEventListener('click',(e) =>{
-    e.preventDefault();
-    console.log(forget_link);
-    location.href = 'forgotPassword.html';
-})  
